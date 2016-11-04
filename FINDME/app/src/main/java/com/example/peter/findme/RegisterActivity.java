8ae5bf2cd9206567bb28383dc74719ae6ad5ee92
Peter <qnguyen523@gmail.com>
2016-11-04 10:59:41 -0700

    user login, database server still needed

--- FINDME/app/src/main/java/com/example/peter/findme/RegisterActivity.java
+++ FINDME/app/src/main/java/com/example/peter/findme/RegisterActivity.java
@@ -1,13 +1,80 @@
 package com.example.peter.findme;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        // declare EditView
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etUser = (EditText) findViewById(R.id.etUser);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        // declare button
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                final String name = etName.getText().toString();
+                final String username = etUser.getText().toString();
+                final String password = etPassword.getText().toString();
+                final String email = etEmail.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        try
+                        {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success)
+                            {
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }
+                            // failure here
+                            else
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(name, username, email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
--- FINDME/app/src/main/java/com/example/peter/findme/UserAreaActivity.java
+++ FINDME/app/src/main/java/com/example/peter/findme/UserAreaActivity.java
@@ -2,12 +2,19 @@ package com.example.peter.findme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserAreaActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+        // declare variables
+        final EditText etUser = (EditText) findViewById(R.id.etUser);
+        /*final EditText etPassword = (EditText) findViewById(R.id.etPassword);*/
+        final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
     }
 }
